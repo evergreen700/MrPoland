cd710c0c79287402c03ffc12e356ad1adc315ded
DSROKKAKU\Dominic <dominic@gnsd.net>
2017-12-22 23:50:29 -0800

    Changed camera, jump, fixed double jump.

--- MrPoland/Assets/Code/PlayerController.cs
+++ MrPoland/Assets/Code/PlayerController.cs
@@ -10,10 +10,10 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed = 10f;
     public float jumpForce = 1000f;
     public Transform groundCheck;
-    public bool isGrounded = true;
+    public bool isGrounded = false;
     public Vector3 jumpv;
 
-    private bool grounded = false;
+
     private Animator anim;
     private Rigidbody2D rb2d;
     
@@ -28,20 +28,20 @@ public class PlayerController : MonoBehaviour
     }
     private void Start()
     {
-        jumpv = new Vector3(0.0f, 27.0f, 0.0f);
+        jumpv = new Vector3(0.0f, 80.0f, 0.0f);
     }
     void OnCollisionStay2D()
     {
         isGrounded = true;
     }
+    private void OnCollisionExit2D()
+    {
+        isGrounded = false;
+    }
     // Update is called once per frame
     void Update()
     {
-          if (Input.GetKeyDown("space") && isGrounded)
-          {
-            rb2d.AddForce(jumpv, ForceMode2D.Impulse);
-            isGrounded = false;
-          }
+
     }
 
     void FixedUpdate()
@@ -61,14 +61,12 @@ public class PlayerController : MonoBehaviour
         else if (h < 0 && facingRight)
             Flip();
 
-        if (jump)
+        if (Input.GetKeyDown("space") && isGrounded)
         {
-            anim.SetTrigger("Jump");
-            rb2d.AddForce(new Vector2(0f, jumpForce));
-            jump = false;
+            rb2d.AddForce(jumpv, ForceMode2D.Impulse);
+            isGrounded = false;
         }
     }
-
 
     void Flip()
     {
---
+++ MrPoland/Assets/Code/doorLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorLogic : MonoBehaviour {
+    GameObject door;
+    GameObject opendoor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        door.SetActive(false);
+        opendoor.SetActive(true);
+    }
+}
