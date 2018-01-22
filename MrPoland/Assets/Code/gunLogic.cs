a5315a886e8059a377372c6c0a93dba4d205bdbd
Dominic <dominic@gnsd.net>
2018-01-21 22:55:25 -0800

    Added new logic for gun rotation, need debug.

--- MrPoland/Assets/Code/gunLogic.cs
+++ MrPoland/Assets/Code/gunLogic.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class gunLogic : MonoBehaviour {
     public GameObject player;
     public GameObject gun;
-    public Vector3 position;
+    Vector3 position;
+    public float speed = 5f;
+
 	// Use this for initialization
 	void Start () {
-        position = new Vector3(3.0f, 0.0f, 0.0f);
+        position = new Vector3(-0.5f, -0.5f, 0.0f);
 
     }
 	
 	// Update is called once per frame
 	void Update () {
         gun.transform.position = player.transform.position + position;
+    }
+
+    void mouseControl()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - gun.transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        gun.transform.rotation = Quaternion.Slerp(gun.transform.rotation, rotation, speed * Time.deltaTime);
     }
 }
