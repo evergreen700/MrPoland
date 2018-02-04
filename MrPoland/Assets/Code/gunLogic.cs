dc939f76483ad2232951a5e0b6c9e1312b0b3d62
DSROKKAKU\Dominic <dominic@gnsd.net>
2018-02-04 00:16:59 -0800

    Added arm rotation

--- MrPoland/Assets/Code/gunLogic.cs
+++ MrPoland/Assets/Code/gunLogic.cs
@@ -8,6 +8,13 @@ public class gunLogic : MonoBehaviour {
     Vector3 position;
     public float speed = 5f;
 
+
+    //shooting vars
+    public float fireRate = 0;
+    public float damage = 10;
+    public LayerMask notToHit;
+
+    float timeToFire = 0;
 	// Use this for initialization
 	void Start () {
         position = new Vector3(-0.5f, -0.5f, 0.0f);
@@ -17,13 +24,36 @@ public class gunLogic : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         gun.transform.position = player.transform.position + position;
+        mouseControl();
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButton ("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
     }
 
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 fire = new Vector2(gun.transform.position.x, gun.transform.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(fire, mousePosition - fire, 100, notToHit);
+    }
     void mouseControl()
     {
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - gun.transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         gun.transform.rotation = Quaternion.Slerp(gun.transform.rotation, rotation, speed * Time.deltaTime);
+        
     }
 }
