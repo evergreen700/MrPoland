37dfbd97852ee6cc42f2bae61f973b0ae3ff50d2
DSROKKAKU\Dominic <dominic@gnsd.net>
2018-02-05 22:47:11 -0800

    Now with bullets

--- MrPoland/Assets/Code/gunLogic.cs
+++ MrPoland/Assets/Code/gunLogic.cs
@@ -5,56 +5,54 @@ using UnityEngine;
 public class gunLogic : MonoBehaviour {
     public GameObject player;
     public GameObject gun;
+    public GameObject bullet;
+    public GameObject bulletClone;
     Vector3 position;
     public float speed = 5f;
-
+    private float timeCount;
 
     //shooting vars
     public float fireRate = 0;
     public float damage = 10;
     public LayerMask notToHit;
+    public bool coll = false;
 
     float timeToFire = 0;
 	// Use this for initialization
 	void Start () {
         position = new Vector3(-0.5f, -0.5f, 0.0f);
-
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
+
         gun.transform.position = player.transform.position + position;
         mouseControl();
-        if (fireRate == 0)
+        if(coll)
         {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                Shoot();
-            }
+            Destroy(bulletClone);
         }
-        else
-        {
-            if (Input.GetButton ("Fire1") && Time.time > timeToFire)
-            {
-                timeToFire = Time.time + 1 / fireRate;
-                Shoot();
-            }
-        }
-    }
-
-    void Shoot()
-    {
-        Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-        Vector2 fire = new Vector2(gun.transform.position.x, gun.transform.position.y);
-        RaycastHit2D hit = Physics2D.Raycast(fire, mousePosition - fire, 100, notToHit);
-        Debug.DrawLine(gun.transform.position, (mousePosition - fire) * 100, Color.cyan);
-    }
+     }
     void mouseControl()
     {
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - gun.transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         gun.transform.rotation = Quaternion.Slerp(gun.transform.rotation, rotation, speed * Time.deltaTime);
+        if (Input.GetButton("Fire1"))
+        {
+            bulletLogic(rotation);
+        }
+    }
+    void bulletLogic(Quaternion rotation)
+    {
+        bulletClone = Instantiate(bullet, transform.position, Quaternion.Slerp(gun.transform.rotation, rotation, speed * Time.deltaTime));
+        bulletClone.GetComponent<Rigidbody2D>().AddForce(transform.right * 5000);
         
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        coll = true;
     }
 }
